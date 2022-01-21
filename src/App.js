ab9052f77382bbc968ac31fc1230e1b2dd4851f0
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-01-21 20:46:45 +0900

    Added bootstrap CSS to my project

--- src/App.js
+++ src/App.js
@@ -3,7 +3,9 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">My Travel Journal</header>
+      <div className="container">
+        <header className="App-header">My Travel Journal</header>
+      </div>
     </div>
   );
 }
