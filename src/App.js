3d3a010a3ac472fdfc9c786722d0dfd387ca1d88
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-01-23 12:31:30 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
+import Navbar from "./components/Navbar";
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <header className="App-header">My Travel Journal</header>
+        <Navbar />
       </div>
     </div>
   );
