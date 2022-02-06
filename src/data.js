53b5a197214690db15b53776fd9fad0c50fcb61e
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-02-06 14:50:20 +0900

    Created data.js component

---
+++ src/data.js
@@ -0,0 +1,46 @@
+export default [
+  {
+    id: 1,
+    title: "Daisetsuzan National Park",
+    location: "Kamikawa, Hokkaido",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Daisetsuzan+National+Park/@43.5083786,142.8442812,11z/data=!4m9!1m2!2m1!1sdaisetsuzan!3m5!1s0x5f0d2938f602a549:0xa0daa8c12d0764fc!8m2!3d43.5083823!4d142.9843628!15sCgtkYWlzZXRzdXphbloNIgtkYWlzZXRzdXphbpIBDW5hdGlvbmFsX3Bhcms",
+    dateVisited: "Jun 23-24, 2018",
+    description:
+      "This was such a fun trip, never mind that it was freezing cold at the top of the mountain in June! The onsen (hot spting) bath made the whole climb worth it at the end of the day. Best time to visit, I hear, is around the second half of September. Watch out for the bears!",
+    coverImage: "daisetsu-zan.jpg",
+  },
+  {
+    id: 2,
+    title: "Nakafurano flower Park",
+    location: "Furano, Hokkaido",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Nakafurano+Flower+Park/@43.4120612,142.4225922,17z/data=!4m6!3m5!1s0x5f0cb49dc5c20959:0xc87051fb00504aed!4b1!8m2!3d43.4120612!4d142.4225922?hl=en",
+    dateVisited: "Jul 19, 2018",
+    description:
+      "Lovely flowers as far as the eyes can see. This was such a relaxing trip. Who knew that that just walking around and taking in the beautiful flowery scenery and scent could be so therapeutic!",
+    coverImage: "flower-garden.jpg",
+  },
+  {
+    id: 3,
+    title: "Tokyo Sky Tree",
+    location: "Sumida City, Tokyo",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Tokyo+Skytree/@35.6939635,139.6959799,12z/data=!3m1!5s0x60188ed6f9e7acb7:0x486cece4c5ab3730!4m9!1m2!2m1!1stokyo+sky+tree+observatory+floor!3m5!1s0x60188ed0d12f9adf:0x7d1d4fb31f43f72a!8m2!3d35.7100627!4d139.8107004!15sCiB0b2t5byBza3kgdHJlZSBvYnNlcnZhdG9yeSBmbG9vcloiIiB0b2t5byBza3kgdHJlZSBvYnNlcnZhdG9yeSBmbG9vcpIBEG9ic2VydmF0aW9uX2RlY2uaASRDaGREU1VoTk1HOW5TMFZKUTBGblNVTnRkWEpIT1hGQlJSQUI",
+    dateVisited: "Feb 9, 2019",
+    description:
+      "From Tokyo sky tree, to Asakusa temple and the famous Shibuya crossing! This was a nice one day trip. Be sure to go up the Tokyo sky tree observatory, the view from the top is awesome.",
+    coverImage: "hello-Tokyo.jpg",
+  },
+  {
+    id: 4,
+    title: "Cape Kamui",
+    location: "Shakotan, Hokkaido",
+    googleMapsUrl:
+      "https://www.google.com/maps/place/Cape+Kamui+(Kamui+Misaki)/@43.3343437,140.3376193,15z/data=!4m13!1m7!3m6!1s0x5f0a5c72c32397e7:0xf2b6b1dde25bb94f!2sCape+Kamui!3b1!8m2!3d43.3343289!4d140.3463741!3m4!1s0x5f0a5c72ca0efc55:0xcc31199ca8f960ae!8m2!3d43.3335323!4d140.3463829",
+    dateVisited: "Sep 22, 2020",
+    description:
+      "I enjoyed walking on the nature trail and viewing the bautiful scenery. You can enjoy the famous Shakotan Blue ocean and view the stunning penninsula and walk through some fantastic views. You can also try some local seafood to make the most of the experience. ",
+    coverImage: "shakotan-blue.jpg",
+  },
+];
