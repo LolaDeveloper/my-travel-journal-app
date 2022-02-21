f9719e9d0b023f7cd95c95a0bc690a7e1c4380e1
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-02-21 21:50:15 +0900

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,22 @@
 import React from "react";
 import "./Navbar.css";
-import Japan from "../images/japan.png";
 
 export default function Navbar() {
   return (
     <nav className="Navbar">
       <div className="Navcontent">
         <h1>
-          <img src={Japan} className="img-fluid flag" alt="flag of Japan" />{" "}
+          <img
+            src="../images/japan.png"
+            className="img-fluid flag"
+            alt="flag of Japan"
+          />{" "}
           Travelling Japan{" "}
-          <img src={Japan} className="img-fluid flag" alt="flag of Japan" />
+          <img
+            src="../images/japan.png"
+            className="img-fluid flag"
+            alt="flag of Japan"
+          />
         </h1>
         <h3>My travel photo diary</h3>
       </div>
