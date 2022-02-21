75d38ccc0752b2ad736bc450100d8768a63392ef
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-02-21 21:49:30 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
+import data from "./data.js";
 import Card from "./components/Card";
 import "./App.css";
 
 export default function App() {
+  const locations = data.map((location) => {
+    return <Card key={location.id} {...location} />;
+  });
   return (
     <div className="App">
       <Navbar />
-      <Hero />
-      <Card />
+      <div className="card-body">{locations}</div>
     </div>
   );
 }
