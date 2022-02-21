8f225dad8b522b3228d1cf25645589393979e7cd
LolaDeveloper <82631715+LolaDeveloper@users.noreply.github.com>
2022-02-21 21:50:00 +0900

    Update card component and styling

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,43 +1,38 @@
 import React from "react";
-import Daisetsu from "../images/daisetsu-zan.jpg";
-import Location from "../images/location-pin.png";
 import "./card.css";
 
-export default function Card() {
+export default function Card(props) {
   return (
-    <div className=" container Card">
-      <img
-        src={Daisetsu}
-        alt="National Park Japan"
-        className=" img-fluid card--image"
-      />
-      <div className="card--stats">
-        <img src={Location} alt="Location Pin" className="card--pin" />
-        <div className="location">
-          <span> Kamikawa, Hokkaido</span>{" "}
-          <span>
-            <a
-              href="https://www.google.com/maps/place/Daisetsuzan+National+Park/@43.5083786,142.8442812,11z/data=!4m9!1m2!2m1!1sdaisetsuzan!3m5!1s0x5f0d2938f602a549:0xa0daa8c12d0764fc!8m2!3d43.5083823!4d142.9843628!15sCgtkYWlzZXRzdXphbloNIgtkYWlzZXRzdXphbpIBDW5hdGlvbmFsX3Bhcms"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              View on Google Maps
-            </a>{" "}
-          </span>
-        </div>
-        <div>
-          <h1 className="class--title">Daisetsuzan National Park </h1>
-          <p className="card--desc">
-            {" "}
-            This was such a fun trip, never mind that it was freezing cold at
-            the top of the mountain in June! The onsen (hot spting) bath made
-            the whole climb worth it at the end of the day. Best time to visit,
-            I hear, is around the second half of September. Watch out for the
-            bears!{" "}
-          </p>
+    <div className="Container">
+      <div className="ImageWrapper">
+        <div className="CardImage">
+          <img
+            src={`../images/${props.coverImage}`}
+            alt={`This is ${props.title}`}
+          />
         </div>
       </div>
-      <hr />
+      <div className="CardInfo">
+        <span>
+          <img
+            src="../images/location-pin.png"
+            alt="location marker"
+            className="location-pin"
+          />{" "}
+          {props.location}{" "}
+        </span>
+        <a
+          className="google-maps-url"
+          target="_blank"
+          rel="noreferrer"
+          href={props.googleMapsUrl}
+        >
+          View on Google Maps
+        </a>
+        <h2>{props.title} </h2>
+        <h5>{props.dateVisited}</h5>
+        <p>{props.description}</p>
+      </div>
     </div>
   );
 }
